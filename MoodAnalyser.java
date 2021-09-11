e087e7c31bbab429d0c1e00ee0104ce4b2927263
abhishekpachkawade <abhishekpachkawade@gmail.com>
2021-09-11 21:07:29 +0530

    Customized Exception Handling Created

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -18,19 +18,18 @@ public class MoodAnalyser {
 	public String analyserMood() throws MoodAnalyserException {
 
 		// Handles Exceptions using try catch block
-		try
-        {
-            if(message.length() == 0)
-            {
-                throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.ENTERED_EMPTY,"Please enter valid message");
-            }
-            if (message.contains("sad"))
-                return "sad";
-            else
-                return "Happy";
-        } catch (NullPointerException e )
-        {
-            throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.ENTERED_NULL,"Please enter valid message");
-        }
+		try {
+			if (message.length() == 0) {
+				throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.ENTERED_EMPTY,
+						"Please enter valid message");
+			}
+			if (message.contains("sad"))
+				return "sad";
+			else
+				return "Happy";
+		} catch (NullPointerException e) {
+			throw new MoodAnalyserException(MoodAnalyserException.ExceptionType.ENTERED_NULL,
+					"Please enter valid message");
+		}
 	}
 }
