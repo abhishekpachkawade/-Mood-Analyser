5974fc2a22e95396e8d71dcbd539abbd2c07bc5e
abhishekpachkawade <abhishekpachkawade@gmail.com>
2021-09-11 19:34:29 +0530

    Refactored code with adding constructor

--- MoodAnalyser.java
+++ MoodAnalyser.java
@@ -2,7 +2,19 @@ package com.bl.abhishek;
 
 public class MoodAnalyser {
 
-	//method analysermood with passing parameter 
+	// private variable
+	private String message;
+
+	// Constructor
+	public MoodAnalyser() {
+	}
+
+	// Constructor one parameter
+	public MoodAnalyser(String message) {
+		this.message = message;
+	}
+
+	// method analysermood with passing parameter
 	public String analyserMood(String message) {
 		if (message.contains("sad")) {
 			return "Sad";
